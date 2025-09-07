23ee552a44eb5bc1bbaac8213ca50e7c5f40436b
dmitriich <chdmitry2@gmail.com>
2025-09-07 16:41:02 +0300

    fix: func sig types

--- project/graph_utils.py
+++ project/graph_utils.py
@@ -17,18 +17,18 @@ def _get_graph_info_by_graph(graph: MultiDiGraph) -> GraphInfo:
         labels=set(nx.get_edge_attributes(graph, "label").values())
     )
 
-def _get_graph_by_name(name) -> MultiDiGraph:
+def _get_graph_by_name(name: str) -> MultiDiGraph:
     graph_path = download(name)
     graph = graph_from_csv(graph_path)
 
     return graph
 
-def get_graph_info_by_name(name):
+def get_graph_info_by_name(name: str) -> GraphInfo:
     graph = _get_graph_by_name(name)
 
     return _get_graph_info_by_graph(graph)
 
-def create_and_save_2_cycle_labeled_graph(n1: int, n2: int, label1: str, label2: str, path: str):
+def create_and_save_2_cycle_labeled_graph(n1: int, n2: int, label1: str, label2: str, path: str) -> None:
     graph = labeled_two_cycles_graph(n1, n2, labels=(label1, label2))
 
     pydot_graph = to_pydot(graph)
